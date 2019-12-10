0020da5060fc634021d5635e09a8755ae2b706b8
Enrique Cazares <cazaresenrique97@gmail.com>
2019-12-09 22:21:36 -0600

    handle auth

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://Enrique:sho0ter42561@storybooks-pjkzs.mongodb.net/test?retryWrites=true&w=majority',
+    googleClientID: '789250537108-5jtrj9ln1rl76k1o2a1jjbont98lhajv.apps.googleusercontent.com',
+    googleClientSecret: 'IPcTrTSuLP9YDllwUMv13Jom'
+}
